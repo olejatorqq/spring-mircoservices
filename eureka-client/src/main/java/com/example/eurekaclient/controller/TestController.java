1e6e45dc976f7905b21cd77ffe0429544bbb3a98
olejatorqq <oleg.orlowsky@gmail.com>
2023-07-08 14:01:21 +0300

    Added gateway routes

--- eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.example.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
-    @GetMapping("test")
+    @GetMapping("/test")
     public String test(){
         return "hello";
     }
